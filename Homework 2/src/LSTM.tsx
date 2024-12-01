84857aa2d4b872db0b89344c63c21d1028c470f4
Ana <116200168+akolovska@users.noreply.github.com>
2024-12-01 02:19:50 +0100

    Add files via upload

---
+++ Homework 2/src/LSTM.tsx
@@ -0,0 +1,7 @@
+function LSTM() {
+    return (
+        <h1>LSTM</h1>
+    )
+}
+
+export default LSTM
---
+++ Homework 2/src/TechnicalAnalysis.tsx
@@ -0,0 +1,7 @@
+function TechnicalAnalysis() {
+    return (
+        <h1>TechnicalAnalysis</h1>
+    )
+}
+
+export default TechnicalAnalysis
